f8ee8c6e374947ac6c55c9eb21f818063a2f1ad7
lavanya9100 <lavanyanaveen55@gmail.com>
2018-03-16 12:40:32 +0530

    5th commit

---
+++ Java_multiplay/src/multiplay/Multiplay.java
@@ -0,0 +1,15 @@
+package multiplay;
+
+public class Multiplay {
+
+	public static void main(String[] args) {
+		int a=12;
+		int b=15;
+		int mul;
+		mul=a*b;
+		System.out.println(mul);
+		
+
+	}
+
+}
